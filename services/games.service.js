adaeba95d6a0271ab91cc7165e5456307fb66906
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2024-12-20 14:07:54 +0700

    chore: added games service

---
+++ services/games.service.js
@@ -0,0 +1,50 @@
+const repo = require("../repositories/games.repository")
+const userRepo = require("../repositories/users.repository")
+
+const createGame = async (game) => {
+    try {
+        const user = await userRepo.getUser(game.player_1)
+        if (!user) {
+            throw new Error("Player 1 not found")
+        }
+        if (game.player_2) {
+            const player_2 = await userRepo.getUser(game.player_2)
+            if (!player_2) {
+                throw new Error("Player 2 not found")
+            }
+        }
+        await repo.createGame(game)
+    } catch(error) {
+        throw new Error(error.message)
+    }
+}
+
+const getGameByUserID = async (userID) => {
+    try {
+        const user = await userRepo.getUser(userID)
+        if (!user) {
+            throw new Error("User not found")
+        }
+        const res = await repo.getGameByUserID(userID)
+        if (res.length === 0) {
+            throw new Error("No game found")
+        }
+        return res
+    } catch(error) {
+        throw new Error(error.message)
+    }
+}
+
+const getGameByID = async (id) => {
+    try {
+        const res = await repo.getGameByID(id)
+        if (!res) {
+            throw new Error("Game not found")
+        }
+        return res
+    } catch(error) {
+        throw new Error(error.message)
+    }
+}
+
+module.exports = {createGame, getGameByUserID, getGameByID}
