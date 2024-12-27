8bca3cf4893dbcdc453afca2c752ffd550fec663
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2024-12-27 20:17:00 +0700

    fix: added error logging and fix register

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,17 +1,17 @@
 const pool = require("../db/db");
 
 const createUser = async (user) => {
-    const {nama, username, avatar, password} = user
+    const {nama, username, avatar, password, email} = user
     try {
-        const res = await pool.query('INSERT INTO users (nama, username, avatar, password) VALUES ($1, $2, $3, $4) RETURNING *', [
-            nama, username, avatar, password
+        const res = await pool.query('INSERT INTO users (nama, username, avatar, password, email) VALUES ($1, $2, $3, $4, $5) RETURNING *', [
+            nama, username, avatar, password, email
         ]);
         await pool.query('INSERT INTO users_stats (id_user, longest_win_streak, winnings, loses, longest_lose_streak, is_win_streak, is_lose_streak) VALUES ($1, 0, 0, 0, 0, 0, 0)', [
             res.rows[0].id_user
         ])
         return res.rows[0];
-    } catch {
-        throw new Error("DB Error Occurred")
+    } catch(error) {
+        throw new Error("DB Error Occurred: createUser " + error.message);
     }
 }
 
@@ -28,8 +28,8 @@ const getUser = async (id) => {
     try {
         const res = await pool.query('SELECT * FROM users WHERE id_user = $1', [id]);
         return res.rows[0];
-    } catch {
-        throw new Error("DB Error Occurred")
+    } catch(error) {
+        throw new Error("DB Error Occurred: getUser " + error.message)
     }
 }
 
